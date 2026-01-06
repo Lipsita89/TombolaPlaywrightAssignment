ddf92ab5053279832ae27ec5fa89f28f1710f0ea
lipsitanayak <lipsy.qa@gmail.com>
2026-01-06 00:04:13 +0000

    refactored

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,11 +16,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
-    //viewport:{width:1280,height:720},
-    // viewport: null,
-    // launchOptions: {
-    //   args: ['--start-maximized']
-    // },
+    viewport:{width:1280,height:720},
     ignoreHTTPSErrors: true,
     permissions: ['geolocation'],
   },
